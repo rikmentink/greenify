eda3396d759c4e14622c071685d9f619a442dc86
RobinHana04 <robinhana2004@gmail.com>
2024-04-03 23:31:21 +0200

    Adjusted workflow

--- server/src/test/java/nl/hu/greenify/GreenifyApplicationTests.java
+++ server/src/test/java/nl/hu/greenify/GreenifyApplicationTests.java
@@ -1,13 +1,20 @@
 package nl.hu.greenify;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
+@ContextConfiguration(classes = {GreenifyApplicationTests.TestConfig.class})
 class GreenifyApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void someTest() {
 	}
 
+	@Configuration
+	static class TestConfig {
+		// Define any beans needed for test here
+	}
 }
