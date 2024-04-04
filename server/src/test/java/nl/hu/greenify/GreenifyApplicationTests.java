91ec06236bb19e11bb4a0bef89bb9c858f65eb39
RobinHana04 <robinhana2004@gmail.com>
2024-04-04 15:02:43 +0200

    Small adjustments

--- server/src/test/java/nl/hu/greenify/GreenifyApplicationTests.java
+++ server/src/test/java/nl/hu/greenify/GreenifyApplicationTests.java
@@ -2,24 +2,16 @@ package nl.hu.greenify;
 
 import domain.User;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@ContextConfiguration(classes = {GreenifyApplicationTests.TestConfig.class})
 class GreenifyApplicationTests {
 
 	@Test
 	void toStringTest() { //for PITest purposes, you can remove this
 		User user = new User("John", "Doe", "test@gmail.com");
 		assertEquals("User{firstName='John', lastName='Doe', email='test@gmail.com'}", user.toString());
-	}
-
-	@Configuration
-	static class TestConfig {
-		// Define any beans needed for test here
 	}
 }
