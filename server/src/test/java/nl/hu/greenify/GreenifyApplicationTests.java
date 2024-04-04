8f792ba93427f654227e4c474af953260920dd1d
RobinHana04 <robinhana2004@gmail.com>
2024-04-04 15:01:04 +0200

    Adjusted workflow & added boilerplate code to see if PiTest issue has been solved

--- server/src/test/java/nl/hu/greenify/GreenifyApplicationTests.java
+++ server/src/test/java/nl/hu/greenify/GreenifyApplicationTests.java
@@ -1,16 +1,21 @@
 package nl.hu.greenify;
 
+import domain.User;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ContextConfiguration(classes = {GreenifyApplicationTests.TestConfig.class})
 class GreenifyApplicationTests {
 
 	@Test
-	void someTest() {
+	void toStringTest() { //for PITest purposes, you can remove this
+		User user = new User("John", "Doe", "test@gmail.com");
+		assertEquals("User{firstName='John', lastName='Doe', email='test@gmail.com'}", user.toString());
 	}
 
 	@Configuration
