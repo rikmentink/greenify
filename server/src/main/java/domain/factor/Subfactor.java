9d063c9c7feeb9818b7ce08188b16a1e1b389417
RobinHana04 <robinhana2004@gmail.com>
2024-04-05 12:40:49 +0200

    Adjusted some classes, and added 3 more classes according to the class diagram

--- server/src/main/java/domain/factor/Subfactor.java
+++ server/src/main/java/domain/factor/Subfactor.java
@@ -1,4 +1,27 @@
 package domain.factor;
 
-public class Subfactor {
+import domain.interfaces.IFactor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Subfactor implements IFactor {
+    private Long id;
+    private IFactor parentIFactor;
+    private boolean isParentFactor;
+
+    public Subfactor(IFactor parentIFactor, boolean isParentFactor) {
+        this.parentIFactor = parentIFactor;
+        this.isParentFactor = isParentFactor;
+    }
+
+    @Override
+    public String toString() {
+        return "Subfactor{" +
+                "id=" + id +
+                ", parentFactor=" + parentIFactor +
+                ", isParentFactor=" + isParentFactor +
+                '}';
+    }
 }
