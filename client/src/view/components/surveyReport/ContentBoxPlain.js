e36d764ebef36106a7f4a8df51ac6853b980c643
l_you <youri.levoye@student.hu.nl>
2024-04-09 13:38:03 +0200

    Created a plain content box component that can be used to put any contents in

---
+++ client/src/view/components/surveyReport/ContentBoxPlain.js
@@ -0,0 +1,27 @@
+import {css, html, LitElement} from "lit";
+
+export class ContentBoxPlain extends LitElement {
+    static styles = [
+        css`
+          .container {
+            background-color: #fff;
+            border-radius: 25px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            text-align: center;
+          }
+    `];
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return html`
+            <div class="container">
+                <slot></slot>
+            </div>
+    `
+    }
+}
+
+window.customElements.define('content-box-plain', ContentBoxPlain);
