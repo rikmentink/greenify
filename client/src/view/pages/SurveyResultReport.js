066a11a873db607c05e7a7a3ad315f87a37ab308
l_you <youri.levoye@student.hu.nl>
2024-04-09 13:37:20 +0200

    Created the general setup of the survey result report page, using a grid for the layout which will contain the custom components

---
+++ client/src/view/pages/SurveyResultReport.js
@@ -0,0 +1,63 @@
+import {css, html, LitElement} from "lit";
+import "../components/surveyReport/charts/AgreementPolarChart.js";
+import "../components/surveyReport/ContentBoxPlain.js";
+import "../components/surveyReport/HeaderBox.js";
+
+export class SurveyResultReport extends LitElement {
+  static styles = [
+    css`
+      :host {
+          background-color: #f0f0f0;
+      }
+      h1 {
+        color: green;
+      }
+      h2 {
+        font-size: 15px;
+        margin: 0px;
+      }
+      .grid-container {
+        display: grid;
+        gap: 10px;
+        grid-template-columns: repeat(2, 1fr);
+      }
+      
+      .grid-left-section {
+        display: grid;
+      }
+      
+      .grid-right-section {
+        display: grid;
+      }
+    `];
+
+  constructor() {
+    super();
+    this.chartData = [100, 80, 20, 60, 85, 30];
+    this.chartLabels = ['Kenmerken betrokken', 'De groene interventie', 'Ontwerp', 'De externe omgeving', 'De organisatie', 'Proces'];
+  }
+
+  render() {
+    return html`
+      <h1>Vergroenings rapportage</h1>
+      <div class="grid-container">
+        <div class="grid-left-section">
+          <content-box-plain>
+            <agreement-polar-chart .chartData=${this.chartData} .chartLabels=${this.chartLabels}></agreement-polar-chart>
+          </content-box-plain>
+        </div>
+        
+        <div class="grid-right-section">
+          <header-box>
+            <h2 slot="header">Actiepunten</h2>
+            <div class="header-box-contents">
+            </div>
+          </header-box>
+        </div>
+      </div>
+      
+    `
+  }
+}
+
+window.customElements.define('survey-result-report', SurveyResultReport);
