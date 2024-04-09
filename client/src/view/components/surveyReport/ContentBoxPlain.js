90cbcaa5122fadf6b2f781473b72e820c647897f
l_you <youri.levoye@student.hu.nl>
2024-04-09 15:59:35 +0200

    Adjusted styling of the plain content box to always be 100% in size of what its placed in.

--- client/src/view/components/surveyReport/ContentBoxPlain.js
+++ client/src/view/components/surveyReport/ContentBoxPlain.js
@@ -7,6 +7,8 @@ export class ContentBoxPlain extends LitElement {
             background-color: #fff;
             border-radius: 25px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            width: 100%;
+            height: 100%;
           }
     `];
 
