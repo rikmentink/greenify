d375e482ba2b1f28b2dd645f8a21b4d1b6e018a5
BeMounir <mounir.bekkaoui@student.hu.nl>
2024-04-10 13:38:08 +0200

    -added overzicht -added vragenlijst (these are both for testing so they will be removed from the navbar later on) -added basic structure ti vragenlijst. -added them to the router

--- client/src/view/router/router.js
+++ client/src/view/router/router.js
@@ -1,6 +1,8 @@
 import { Router } from '@vaadin/router';
 
 import { Register } from '../pages/Register';
+import { Vragenlijst } from '../pages/Vragenlijst';
+import { Overzicht } from '../pages/Overzicht';
 
 export const router = new Router(document.getElementById('outlet'), {
     baseUrl: import.meta.env.BASE_URL
@@ -14,6 +16,14 @@ router.setRoutes([
     {
         path: import.meta.env.BASE_URL + 'register',
         component: 'gi-register',
+    },
+    {
+        path: import.meta.env.BASE_URL + 'vragenlijst',
+        component: 'gi-vragenlijst',
+    },
+    {
+        path: import.meta.env.BASE_URL + 'overzicht',
+        component: 'gi-overzicht',
     },
 ]);
 
