db3abdf2bea88ad74733f2efe74a5716b1316fae
rikmentink <rik.mentink@icloud.com>
2024-04-11 11:09:03 +0200

    Created Template class.

--- server/src/main/java/domain/Template.java
+++ server/src/main/java/domain/Template.java
@@ -1,4 +1,72 @@
 package domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Template {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private Integer version;
+
+    public Template(Long id, String name, String description, Integer version) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.version = version;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Template other = (Template) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (version == null) {
+            if (other.version != null)
+                return false;
+        } else if (!version.equals(other.version))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Template [id=" + id + ", name=" + name + ", description=" + description + ", version=" + version + "]";
+    }
 }
