adb945b9eeee259503fd77788514c583be30df01
BeMounir <mounir.bekkaoui@student.hu.nl>
2024-04-11 11:56:45 +0200

    -added button component -added radiobutton component -changes to vragenlijst

--- client/src/view/pages/Vragenlijst.js
+++ client/src/view/pages/Vragenlijst.js
@@ -1,13 +1,14 @@
 import { LitElement, html, css } from 'lit';
+import '../components/buttons/radiobutton.js';
 
 export class Vragenlijst extends LitElement {
-    static styles = [css`
-      h1 {
+    static styles = css`
+      h1, p {
         color: black;
       }
 
       .back-button {
-        color: greenyellow;
+        color: #4CBB17;
       }
 
       .question-container {
@@ -38,19 +39,29 @@ export class Vragenlijst extends LitElement {
       }
 
       hr.divider {
-        border-top: 3px solid #bbb;
+        border-top: 3px solid #000000;
         margin-left: 20px;
         min-height: 50px;
-        flew-grow: 1;
+        flex-grow: 1;
         width: 0px;
       }
-    `];
+    `;
 
-    static properties = {
-    };
+    static properties = {};
 
     constructor() {
         super();
+    }
+
+    handleRadioChange(event) {
+        const radioButton = event.target;
+        const allRadios = this.shadowRoot.querySelectorAll(`radio-button[name="${radioButton.name}"]`);
+
+        allRadios.forEach(radio => {
+            if (radio !== radioButton) {
+                radio.checked = false;
+            }
+        });
     }
 
     render() {
@@ -60,113 +71,42 @@ export class Vragenlijst extends LitElement {
             <div class="question-container">
                 <p class="question">1. Deze vragenlijst gaat over de groene interventie.</p>
                 <div class="radio-container">
-                    <div class="radio-button">
-                        <label>1</label>
-                        <input type="radio" name="question1">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>2</label>
-                        <input type="radio" name="question1">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>3</label>
-                        <input type="radio" name="question1">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>4</label>
-                        <input type="radio" name="question1">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>5</label>
-                        <input type="radio" name="question1">
-                        <span class="checkmark"></span>
-                    </div>
+                    <radio-button label="1" name="question1" value="1" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="2" name="question1" value="2" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="3" name="question1" value="3" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="4" name="question1" value="4" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="5" name="question1" value="5" @change="${this.handleRadioChange}"></radio-button>
+                </div>
 
-                    <hr class="divider">
+                <hr class="divider">
 
-                    <div class="priority-container">
-                        <div class="radio-button">
-                            <label>1</label>
-                            <input type="radio" name="priority">
-                            <span class="checkmark"></span>
-                        </div>
-                        <div class="radio-button">
-                            <label>2</label>
-                            <input type="radio" name="priority">
-                            <span class="checkmark"></span>
-                        </div>
-                        <div class="radio-button">
-                            <label>3</label>
-                            <input type="radio" name="priority">
-                            <span class="checkmark"></span>
-                        </div>
-                        <div class="radio-button">
-                            <label>4</label>
-                            <input type="radio" name="priority">
-                            <span class="checkmark"></span>
-                        </div>
-                    </div>
+                <div class="priority-container">
+                    <radio-button label="1" name="priority1" value="1" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="2" name="priority1" value="2" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="3" name="priority1" value="3" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="4" name="priority1" value="4" @change="${this.handleRadioChange}"></radio-button>
                 </div>
             </div>
 
             <div class="question-container">
                 <p class="question">2. Dit is een tweede test vraag.</p>
                 <div class="radio-container">
-                    <div class="radio-button">
-                        <label>1</label>
-                        <input type="radio" name="question2">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>2</label>
-                        <input type="radio" name="question2">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>3</label>
-                        <input type="radio" name="question2">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>4</label>
-                        <input type="radio" name="question2">
-                        <span class="checkmark"></span>
-                    </div>
-                    <div class="radio-button">
-                        <label>5</label>
-                        <input type="radio" name="question2">
-                        <span class="checkmark"></span>
-                    </div>
-
-                    <hr class="divider">
-
-                    <div class="priority-container">
-                        <div class="radio-button">
-                            <label>1</label>
-                            <input type="radio" name="priority2">
-                            <span class="checkmark"></span>
-                        </div>
-                        <div class="radio-button">
-                            <label>2</label>
-                            <input type="radio" name="priority2">
-                            <span class="checkmark"></span>
-                        </div>
-                        <div class="radio-button">
-                            <label>3</label>
-                            <input type="radio" name="priority2">
-                            <span class="checkmark"></span>
-                        </div>
-                        <div class="radio-button">
-                            <label>4</label>
-                            <input type="radio" name="priority2">
-                            <span class="checkmark"></span>
-                        </div>
-                    </div>
+                    <radio-button label="1" name="question2" value="1" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="2" name="question2" value="2" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="3" name="question2" value="3" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="4" name="question2" value="4" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="5" name="question2" value="5" @change="${this.handleRadioChange}"></radio-button>
                 </div>
+
+                <hr class="divider">
+
+                <div class="priority-container">
+                    <radio-button label="1" name="priority2" value="1" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="2" name="priority2" value="2" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="3" name="priority2" value="3" @change="${this.handleRadioChange}"></radio-button>
+                    <radio-button label="4" name="priority2" value="4" @change="${this.handleRadioChange}"></radio-button>
+                </div>
+            </div>
         `;
     }
 }
