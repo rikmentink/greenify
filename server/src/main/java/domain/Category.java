c6040dba190c2f0084400e66578f57858a8a94ab
rikmentink <rik.mentink@icloud.com>
2024-04-11 11:12:14 +0200

    Created Category class and improved Template

--- server/src/main/java/domain/Category.java
+++ server/src/main/java/domain/Category.java
@@ -1,4 +1,26 @@
 package domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Category {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String color;
+    private String description;
+    
+    public Category(Long id, String name, String color, String description) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.description = description;
+    }
 }
--- server/src/main/java/domain/Template.java
+++ server/src/main/java/domain/Template.java
@@ -1,12 +1,17 @@
 package domain;
 
-import jakarta.persistence.Entity;
+import java.util.List;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
-@Entity
 @Getter
+@ToString
+@EqualsAndHashCode
 public class Template {
     @Id
     @GeneratedValue
@@ -15,58 +20,14 @@ public class Template {
     private String description;
     private Integer version;
 
-    public Template(Long id, String name, String description, Integer version) {
+    @OneToMany
+    private List<Category> questions;
+
+    public Template(Long id, String name, String description, Integer version, List<Category> questions) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.version = version;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + ((version == null) ? 0 : version.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Template other = (Template) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        if (version == null) {
-            if (other.version != null)
-                return false;
-        } else if (!version.equals(other.version))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Template [id=" + id + ", name=" + name + ", description=" + description + ", version=" + version + "]";
+        this.questions = questions;
     }
 }
