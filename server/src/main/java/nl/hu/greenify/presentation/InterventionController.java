8f76f3d20e9015c3bc33696b4d338b76c31805ca
RobinHana04 <robinhana2004@gmail.com>
2024-04-12 15:32:32 +0200

    Made InterventionController

---
+++ server/src/main/java/nl/hu/greenify/presentation/InterventionController.java
@@ -0,0 +1,39 @@
+package nl.hu.greenify.presentation;
+
+import nl.hu.greenify.application.InterventionService;
+import nl.hu.greenify.domain.Person;
+import nl.hu.greenify.domain.enums.PhaseName;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/intervention")
+public class InterventionController {
+    private final InterventionService interventionService;
+
+    public InterventionController(InterventionService interventionService) {
+        this.interventionService = interventionService;
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> addIntervention(@RequestParam Long id, String name, String description) {
+       try {
+           Person person = this.interventionService.getPersonById(id);
+           this.interventionService.addIntervention(name, description, person);
+           return ResponseEntity.ok(person.getInterventions());
+       } catch (IllegalArgumentException e) {
+           return ResponseEntity.badRequest().body(e.getMessage());
+       }
+    }
+
+    @PostMapping("/phase/{personId}")
+    public ResponseEntity<?> addPhaseToIntervention(@RequestParam Long personId, String name, PhaseName phaseName) {
+       try {
+           Person person = this.interventionService.getPersonById(personId);
+           this.interventionService.addPhaseToIntervention(name, phaseName, person);
+           return ResponseEntity.ok(person.getInterventions());
+       } catch (IllegalArgumentException e) {
+           return ResponseEntity.badRequest().body(e.getMessage());
+       }
+    }
+}
