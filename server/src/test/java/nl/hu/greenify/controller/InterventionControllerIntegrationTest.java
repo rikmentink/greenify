2bf7315db182834a35468fdde20759b21e46c289
RobinHana04 <robinhana2004@gmail.com>
2024-04-12 15:35:55 +0200

    Made setup for InterventionDto Made setup for ControllerIntegrationTest

---
+++ server/src/test/java/nl/hu/greenify/controller/InterventionControllerIntegrationTest.java
@@ -0,0 +1,22 @@
+package nl.hu.greenify.controller;
+
+import nl.hu.greenify.data.GreenifyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class InterventionControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private GreenifyRepository greenifyRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+}
