80501d5a42dc6a87a61770f42ea85dcd939fdcce
l_you <youri.levoye@student.hu.nl>
2024-04-12 14:24:40 +0200

    Created initial setup of report interface and classes that implement the interface.

--- server/src/main/java/nl/hu/greenify/domain/interfaces/Report.java
+++ server/src/main/java/nl/hu/greenify/domain/interfaces/Report.java
@@ -1,4 +1,12 @@
 package nl.hu.greenify.domain.interfaces;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 public interface Report {
+    void generateReport();
+    double calculateAgreementScore();
+    List<String> getActionPoints();
+    List<String> getComments();
+
 }
