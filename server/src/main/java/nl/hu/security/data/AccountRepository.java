83321451b804e3131464ff84484c3f22df868f26
Joostb26 <bos.joost@outlook.com>
2024-04-13 10:24:36 +0200

    AccountRepository for database

---
+++ server/src/main/java/nl/hu/security/data/AccountRepository.java
@@ -0,0 +1,9 @@
+package nl.hu.security.data;
+
+import nl.hu.security.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
+}
