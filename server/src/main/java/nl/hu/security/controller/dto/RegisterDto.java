fde885895b380da5bc9ae8610aee4f6b4c6376e9
Joostb26 <bos.joost@outlook.com>
2024-04-13 10:27:51 +0200

    Added RegisterDto

---
+++ server/src/main/java/nl/hu/security/controller/dto/RegisterDto.java
@@ -0,0 +1,19 @@
+package nl.hu.security.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+public class RegisterDto {
+    @NotBlank
+    public String email;
+
+    @Size(min = 5)
+    public String password;
+
+    @NotBlank
+    public String firstName;
+
+    @NotBlank
+    public String lastName;
+}
