2c38ee2591e608637b81f5abdd3a4d325db2fbbc
RobinHana04 <robinhana2004@gmail.com>
2024-04-14 21:45:13 +0200

    Adjusted phase functionality according to the use case, and adjusted tests accordingly.

--- server/src/test/java/nl/hu/greenify/core/domain/PersonTest.java
+++ server/src/test/java/nl/hu/greenify/core/domain/PersonTest.java
@@ -100,7 +100,8 @@ public class PersonTest { //Extra tests
     @DisplayName("User should not be able to add a phase with the same name to the same intervention")
     void userAddDuplicatePhaseToIntervention() {
         person.addPhaseToIntervention("Garden", PhaseName.INITIATION);
-        Assertions.assertThrows(IllegalArgumentException.class, () -> person.addPhaseToIntervention("Garden", PhaseName.INITIATION));
+        person.addPhaseToIntervention("Garden", PhaseName.INITIATION);
+        Assertions.assertEquals(2, person.getInterventions().get(0).getPhases().size());
     }
 
 }
