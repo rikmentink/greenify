f93865447ab413bcf90db3b6ff8afd102c9d4196
rikmentink <rik.mentink@icloud.com>
2024-04-16 12:21:51 +0200

    Improved phase repository

--- server/src/main/java/nl/hu/greenify/core/data/PhaseRepository.java
+++ server/src/main/java/nl/hu/greenify/core/data/PhaseRepository.java
@@ -1,11 +1,8 @@
 package nl.hu.greenify.core.data;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import nl.hu.greenify.core.domain.Phase;
 
 public interface PhaseRepository extends JpaRepository<Phase, Long> {
-    // public Optional<Phase> findById(Long id);
 }
