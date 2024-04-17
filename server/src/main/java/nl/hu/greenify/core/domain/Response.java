7e4c3f956fd36ef0358ef70b4bbc33e20f7dd193
l_you <youri.levoye@student.hu.nl>
2024-04-17 16:11:46 +0200

    Created the Response class implementation with attributes, initial values and method to calculate the score. Additionally, added items to both Enums and assigned values to them to allow for very easy adjustments in the future if needed.

--- server/src/main/java/nl/hu/greenify/core/domain/Response.java
+++ server/src/main/java/nl/hu/greenify/core/domain/Response.java
@@ -1,4 +1,40 @@
 package nl.hu.greenify.core.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import nl.hu.greenify.core.domain.enums.FacilitatingFactor;
+import nl.hu.greenify.core.domain.enums.Priority;
+
+@Entity
+@ToString
+@Getter
 public class Response {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private double score;
+
+    @Setter
+    private String comment;
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    private FacilitatingFactor facilitatingFactor;
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    public Response() {
+        this.score = 0;
+        this.facilitatingFactor = FacilitatingFactor.PENDING;
+        this.priority = Priority.PENDING;
+    }
+
+    public void calculateScore() {
+        this.score = facilitatingFactor.getValue() * priority.getValue();
+    }
 }
--- server/src/main/java/nl/hu/greenify/core/domain/enums/FacilitatingFactor.java
+++ server/src/main/java/nl/hu/greenify/core/domain/enums/FacilitatingFactor.java
@@ -1,4 +1,20 @@
 package nl.hu.greenify.core.domain.enums;
 
+import lombok.Getter;
+
 public enum FacilitatingFactor {
+    TOTALLY_DISAGREE(1),
+    DISAGREE(2),
+    NEUTRAL(3),
+    AGREE(4),
+    TOTALLY_AGREE(5),
+    I_DONT_KNOW(0),
+    PENDING(0);
+
+    @Getter
+    private final int value;
+
+    FacilitatingFactor(int value) {
+        this.value = value;
+    }
 }
