be2fd975c9f999d41e6b6238d234c573918e2bff
Joostb26 <bos.joost@outlook.com>
2024-04-19 15:37:19 +0200

    Fixed username to email

--- server/src/main/java/nl/hu/greenify/core/data/GreenifyRepository.java
+++ server/src/main/java/nl/hu/greenify/core/data/GreenifyRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GreenifyRepository extends JpaRepository<Person, Long> {
-    Optional<Person> findByUsername(String username);
+    Optional<Person> findByEmail(String username);
 }
