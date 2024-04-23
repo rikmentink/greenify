92ecfedaad91d1022aa36fb33a202826d520435b
rikmentink <rik.mentink@icloud.com>
2024-04-23 12:35:13 +0200

    Changed DTO contents based on frontend needs

--- server/src/main/java/nl/hu/greenify/core/presentation/dto/QuestionSetDto.java
+++ server/src/main/java/nl/hu/greenify/core/presentation/dto/QuestionSetDto.java
@@ -1,36 +1,27 @@
 package nl.hu.greenify.core.presentation.dto;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import lombok.EqualsAndHashCode;
 import nl.hu.greenify.core.domain.Category;
 import nl.hu.greenify.core.domain.Survey;
 import nl.hu.greenify.core.domain.factor.Factor;
-import nl.hu.greenify.core.domain.factor.IFactor;
-import nl.hu.greenify.core.domain.factor.Subfactor;
 
 @EqualsAndHashCode
 public class QuestionSetDto {
     private Long surveyId;
-    private Long categoryId;
+    private Category category;
     private List<Factor> factors;
-    private List<Subfactor> subfactors;
 
-    public QuestionSetDto(Long surveyId, Long categoryId, List<Factor> factors,
-            List<Subfactor> subfactors) {
+    public QuestionSetDto(Long surveyId, Category category, List<Factor> factors) {
         this.surveyId = surveyId;
-        this.categoryId = categoryId;
+        this.category = category;
         this.factors = factors;
-        this.subfactors = subfactors;
     }
 
     public static QuestionSetDto fromEntity(Survey survey, Long categoryId) {
         Category category = survey.getCategories().stream().filter(c -> c.getId().equals(categoryId)).findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Category not found"));
-        List<Subfactor> subfactors = category.getFactors().stream()
-                .flatMap(f -> f.getSubfactors().stream())
-                .collect(Collectors.toList());
-        return new QuestionSetDto(survey.getId(), categoryId, category.getFactors(), subfactors);
+        return new QuestionSetDto(survey.getId(), category, category.getFactors());
     }
 }
