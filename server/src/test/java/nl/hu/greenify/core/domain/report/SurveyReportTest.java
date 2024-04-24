dd58934e5704e37364e9e31661d51b82ab15ece6
l_you <youri.levoye@student.hu.nl>
2024-04-24 17:29:25 +0200

    Updated test to only have attributes for objects that the test itself actually uses while also expanding the setup itself by creating 2 survey's, each with 2 categories, each with 1 factor that have 2 subfactors. Attributes are used to prepare a subfactor where a response can ve provided on.

--- server/src/test/java/nl/hu/greenify/core/domain/report/SurveyReportTest.java
+++ server/src/test/java/nl/hu/greenify/core/domain/report/SurveyReportTest.java
@@ -12,44 +12,75 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SurveyReportTest {
-
-    private Category category;
-    private Factor factor;
-    private Subfactor subfactor;
-    private Template template;
     private Phase phase;
-    private Subfactor subfactor1;
-    private Subfactor subfactor2;
+    private Subfactor subfactor1Survey1;
+    private Subfactor subfactor2Survey1;
+    private Subfactor subfactor3Survey1;
+    private Subfactor subfactor4Survey1;
+    private Subfactor subfactor1Survey2;
+    private Subfactor subfactor2Survey2;
+    private Subfactor subfactor3Survey2;
+    private Subfactor subfactor4Survey2;
 
     @BeforeEach
     void setUp() {
         // Template creation steps:
-        this.category = new Category(1L, "name", "red", "description");
-        this.factor = new Factor(1L, "title", 1);
-        category.addFactor(factor);
-        this.subfactor = new Subfactor(1L, "title", 1, true);
-        factor.addSubfactor(subfactor);
-        this.template = new Template(1L, "name", "description", 1, List.of(category));
+        // 1. Create categories
+        Category categoryTemplate1 = new Category(1L, "name", "red", "description");
+        Category categoryTemplate2 = new Category(2L, "name2", "blue", "description2");
 
+        // 2. Create factors
+        Factor factorTemplate1 = new Factor(1L, "title", 1);
+        Factor factorTemplate2 = new Factor(2L, "title2", 2);
+
+        // 3. Attach each factor to a category
+        categoryTemplate1.addFactor(factorTemplate1);
+        categoryTemplate2.addFactor(factorTemplate2);
+
+        // 4. Create subfactors
+        Subfactor subfactorTemplate1 = new Subfactor(1L, "title", 1, true);
+        Subfactor subfactorTemplate2 = new Subfactor(2L, "title2", 2, true);
+        Subfactor subfactorTemplate3 = new Subfactor(3L, "title3", 3, false);
+        Subfactor subfactorTemplate4 = new Subfactor(4L, "title4", 4, false);
+
+        // 5. Attach each subfactor to a factor
+        factorTemplate1.addSubfactor(subfactorTemplate1);
+        factorTemplate1.addSubfactor(subfactorTemplate2);
+        factorTemplate2.addSubfactor(subfactorTemplate3);
+        factorTemplate2.addSubfactor(subfactorTemplate4);
+
+        // 6. Create a template with the categories
+        Template template = new Template(1L, "name", "description", 1, List.of(categoryTemplate1, categoryTemplate2));
+
+        // Phase creation to create surveys for based on the template:
         this.phase = new Phase(PhaseName.INITIATION);
 
         // Survey creations based on templates:
         Survey.createSurvey(phase, Template.copyOf(template));
         Survey.createSurvey(phase, Template.copyOf(template));
-        System.out.println(phase.getSurveys());
 
+        // Prepare subfactors that can be used to provide responses on
+        setupProvideResponseSurvey1();
+        setupProvideResponseSurvey2();
     }
 
+    // Helper methods to prepare for responses for the surveys
     void setupProvideResponseSurvey1() {
         Survey survey = phase.getSurveys().get(0);
 
         Category category = survey.getCategories().get(0);
         Factor factor = category.getFactors().get(0);
-        this.subfactor1 = factor.getSubfactors().get(0);
+        this.subfactor1Survey1 = factor.getSubfactors().get(0);
+        this.subfactor2Survey1 = factor.getSubfactors().get(1);
 
+        Category category2 = survey.getCategories().get(1);
+        Factor factor2 = category2.getFactors().get(0);
+        this.subfactor3Survey1 = factor2.getSubfactors().get(0);
+        this.subfactor4Survey1 = factor2.getSubfactors().get(1);
     }
 
     void setupProvideResponseSurvey2() {
@@ -57,29 +88,31 @@ public class SurveyReportTest {
 
         Category category = survey.getCategories().get(0);
         Factor factor = category.getFactors().get(0);
-        this.subfactor2 = factor.getSubfactors().get(0);
+        this.subfactor1Survey2 = factor.getSubfactors().get(0);
+        this.subfactor2Survey2 = factor.getSubfactors().get(1);
 
+        Category category2 = survey.getCategories().get(1);
+        Factor factor2 = category2.getFactors().get(0);
+        this.subfactor3Survey2 = factor2.getSubfactors().get(0);
+        this.subfactor4Survey2 = factor2.getSubfactors().get(1);
     }
+
     @Test
     @DisplayName("When responses are provided, the maximum possible agreement score should be calculated.")
-    void testMaximumPossibleAgreementScore() {
-        // Survey 1:
-        setupProvideResponseSurvey1();
-        Response response = new Response(this.subfactor1);
+    void testMaxScore() {
+        Response response = new Response(this.subfactor1Survey1);
         response.setFacilitatingFactor(FacilitatingFactor.TOTALLY_AGREE);
         response.setPriority(Priority.TOP_PRIORITY);
-        this.subfactor1.setResponse(response);
+        this.subfactor1Survey1.setResponse(response);
 
-        // Survey 2:
-        setupProvideResponseSurvey2();
-        Response response2 = new Response(this.subfactor2);
+        Response response2 = new Response(this.subfactor2Survey1);
         response2.setFacilitatingFactor(FacilitatingFactor.DISAGREE);
         response2.setPriority(Priority.LITTLE_PRIORITY);
-        subfactor2.setResponse(response2);
+        subfactor2Survey1.setResponse(response2);
 
         SurveyReport surveyReport = new SurveyReport(phase);
 
-        assertEquals(20.0, surveyReport.calculateMaximumPossibleScore());
+        assertEquals(20.0, surveyReport.getMaxScore());
     }
 
     @Test
@@ -87,7 +120,7 @@ public class SurveyReportTest {
     void testMaximumPossibleAgreementScoreWithNoResponses() {
         SurveyReport surveyReport = new SurveyReport(phase);
 
-        assertEquals(0.0, surveyReport.calculateMaximumPossibleScore());
+        assertEquals(0.0, surveyReport.getMaxScore());
     }
 }
 
