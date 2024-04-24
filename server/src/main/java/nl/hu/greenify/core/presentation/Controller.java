c3d3e99e0880990ddc827da2c89e0fc79342bf21
rikmentink <rik.mentink@icloud.com>
2024-04-24 09:05:33 +0200

    Implemented controller generalization in intervention and sorted endpoints.

--- server/src/main/java/nl/hu/greenify/core/presentation/Controller.java
+++ server/src/main/java/nl/hu/greenify/core/presentation/Controller.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-public abstract class Controller {
+abstract class Controller {
 
     /**
      * Helper to create proper response entities.
--- server/src/main/java/nl/hu/greenify/core/presentation/InterventionController.java
+++ server/src/main/java/nl/hu/greenify/core/presentation/InterventionController.java
@@ -10,37 +10,22 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/intervention")
-public class InterventionController {
+public class InterventionController extends Controller {
     private final InterventionService interventionService;
 
     public InterventionController(InterventionService interventionService) {
         this.interventionService = interventionService;
     }
-    @PostMapping("/create")
-    public ResponseEntity<?> createIntervention(@RequestParam Long adminId, String name, String description) {
-        try {
-            this.interventionService.createIntervention(name, description, adminId);
-            return ResponseEntity.ok("Intervention created");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
 
-    @PostMapping("/phase/{id}")
-    public ResponseEntity<?> addPhase(@PathVariable Long id, PhaseName phaseName) {
-        try {
-            this.interventionService.addPhase(id, phaseName);
-            return ResponseEntity.ok("Phase added");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
+    /**
+     * Intervention endpoints
+     */
 
-    @GetMapping("/phase/{id}")
-    public ResponseEntity<?> getPhaseById(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getInterventionById(@PathVariable Long id) {
         try {
-            return ResponseEntity.ok(this.interventionService.getPhaseById(id));
-        } catch (PhaseNotFoundException p) {
+            return ResponseEntity.ok(this.interventionService.getInterventionById(id));
+        } catch (InterventionNotFoundException i) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
@@ -54,11 +39,35 @@ public class InterventionController {
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getInterventionById(@PathVariable Long id) {
+    @PostMapping("/create")
+    public ResponseEntity<?> createIntervention(@RequestParam Long adminId, String name, String description) {
         try {
-            return ResponseEntity.ok(this.interventionService.getInterventionById(id));
-        } catch (InterventionNotFoundException i) {
+            this.interventionService.createIntervention(name, description, adminId);
+            return ResponseEntity.ok("Intervention created");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    /**
+     * Phase endpoints
+     */
+
+    @GetMapping("/phase/{id}")
+    public ResponseEntity<?> getPhaseById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(this.interventionService.getPhaseById(id));
+        } catch (PhaseNotFoundException p) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PostMapping("/phase/{id}")
+    public ResponseEntity<?> addPhase(@PathVariable Long id, PhaseName phaseName) {
+        try {
+            this.interventionService.addPhase(id, phaseName);
+            return ResponseEntity.ok("Phase added");
+        } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
