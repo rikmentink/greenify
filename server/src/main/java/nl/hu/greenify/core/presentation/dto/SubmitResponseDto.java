70ca609ac62c9f3da0b195d0f6cc2bde39872307
rikmentink <rik.mentink@icloud.com>
2024-05-14 07:08:07 +0200

    Created response endpoint and suited DTO

---
+++ server/src/main/java/nl/hu/greenify/core/presentation/dto/SubmitResponseDto.java
@@ -0,0 +1,29 @@
+package nl.hu.greenify.core.presentation.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NonNull;
+import nl.hu.greenify.core.domain.enums.FacilitatingFactor;
+import nl.hu.greenify.core.domain.enums.Priority;
+
+@Getter
+public class SubmitResponseDto {
+    @NonNull
+    private Long surveyId;
+
+    @NonNull
+    private Long subfactorId;
+    
+    @JsonProperty("facilitating-factor")
+    private FacilitatingFactor facilitatingFactor;
+    private Priority priority;
+    private String comment;
+    
+    public SubmitResponseDto(Long surveyId, Long subfactorId, FacilitatingFactor facilitatingFactor, Priority priority, String comment) {
+        this.surveyId = surveyId;
+        this.subfactorId = subfactorId;
+        this.facilitatingFactor = facilitatingFactor;
+        this.priority = priority;
+        this.comment = comment;
+    }
+}
