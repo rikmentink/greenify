d332eac0df5e10b763d6d3bc406ca97c9d421fe0
rikmentink <rik.mentink@icloud.com>
2024-05-14 06:57:02 +0200

    Added implementation for response service method

--- client/src/services/SurveyService.js
+++ client/src/services/SurveyService.js
@@ -1,10 +1,12 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 async function getSurvey(id, categoryId, page = 1, pageSize = 1000) {
-    const url = new URL(`${API_URL}/survey/${id}/questions`);
-    url.searchParams.append('categoryId', categoryId);
-    url.searchParams.append('page', page);
-    url.searchParams.append('pageSize', pageSize);
+    // const url = new URL(`${API_URL}/survey/${id}/questions`);
+    // url.searchParams.append('categoryId', categoryId);
+    // url.searchParams.append('page', page);
+    // url.searchParams.append('pageSize', pageSize);
+
+    const url = './survey-demo.json'
 
     return fetch(url, {
         method: 'GET',
@@ -12,7 +14,6 @@ async function getSurvey(id, categoryId, page = 1, pageSize = 1000) {
         headers: { 'Content-Type': 'application/json' },
     }).then(response => response.json())
       .then(data => {
-        console.log('Success:', data);
         return data;
       })
       .catch(error => {
@@ -21,8 +22,21 @@ async function getSurvey(id, categoryId, page = 1, pageSize = 1000) {
 }
 
 async function saveResponse(id, subfactorId, response) {
-  // TODO: Use back-end service!
   console.log(`Saving response for subfactor #${subfactorId} on survey #${id}:`, response);
+  
+  const url = new URL(`${API_URL}/survey/${id}/response`);
+  return fetch(url, {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ subfactorId, response }),
+  }).then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
 } 
 
 export { getSurvey, saveResponse };
