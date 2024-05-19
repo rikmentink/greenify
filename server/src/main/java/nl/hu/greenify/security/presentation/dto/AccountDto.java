065e2f19c55484749194bc34d670b05d6b773b64
Joostb26 <bos.joost@outlook.com>
2024-05-19 20:23:02 +0200

    Added to the AccountDto the roles of the user

--- server/src/main/java/nl/hu/greenify/security/presentation/dto/AccountDto.java
+++ server/src/main/java/nl/hu/greenify/security/presentation/dto/AccountDto.java
@@ -3,16 +3,28 @@ package nl.hu.greenify.security.presentation.dto;
 import lombok.Getter;
 import nl.hu.greenify.core.domain.Person;
 import nl.hu.greenify.security.domain.Account;
+import nl.hu.greenify.security.domain.AccountCredentials;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class AccountDto {
     private final String email;
 
-    public AccountDto(String email) {
+    private final List<GrantedAuthority> authorities;
+
+    public AccountDto(String email, List<GrantedAuthority> authorities) {
         this.email = email;
+        this.authorities = authorities != null ? Collections.unmodifiableList(authorities) : Collections.emptyList();
     }
 
     public static AccountDto fromEntity(Account account) {
-        return new AccountDto(account.getUsername());
+        return new AccountDto(account.getUsername(), account.getAuthorities() != null
+                ? new ArrayList<>(account.getAuthorities())
+                : Collections.emptyList());
     }
 }
