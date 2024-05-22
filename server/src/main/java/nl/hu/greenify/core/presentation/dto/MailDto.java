201d7d77e10ed233032f9bae25f55e8223c39c5e
RobinHana04 <robinhana2004@gmail.com>
2024-05-22 17:31:25 +0200

    Created MailDTO to prevent domain complications

---
+++ server/src/main/java/nl/hu/greenify/core/presentation/dto/MailDto.java
@@ -0,0 +1,12 @@
+package nl.hu.greenify.core.presentation.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MailDto {
+    private String to;
+    private String subject;
+    private String body;
+
+    // getters and setters
+}
