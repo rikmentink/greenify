fe83446093fdb7574f66d6503cc148aa88a83597
RobinHana04 <robinhana2004@gmail.com>
2024-05-22 17:35:08 +0200

    Slight adjustments regarding the return signature of MailService

--- server/src/main/java/nl/hu/greenify/core/application/MailService.java
+++ server/src/main/java/nl/hu/greenify/core/application/MailService.java
@@ -7,18 +7,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MailService {
+
     private final JavaMailSender emailSender;
 
     public MailService(JavaMailSender emailSender) {
         this.emailSender = emailSender;
     }
 
-    public void sendSimpleMessage(String to, String subject, String text) {
+    public SimpleMailMessage sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+
         message.setFrom("info@GreenIT.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+
+        return message;
     }
 }
