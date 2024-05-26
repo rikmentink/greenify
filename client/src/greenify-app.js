aaf49d5ca4028eb3aa3fb8d2c0602dcba857b35a
rikmentink <rik.mentink@icloud.com>
2024-05-26 13:20:51 +0200

    Updated router implementation

--- client/src/greenify-app.js
+++ client/src/greenify-app.js
@@ -1,5 +1,12 @@
 import { LitElement, css, html } from 'lit';
-import { InfoPopUp } from "./view/pages/InfoPopUp.js";
+import { Router } from '@vaadin/router';
+
+// Components
+import { Header } from './view/components/header/Header';
+import { ProfilePanel } from './view/components/profile/ProfilePanel';
+
+// Views
+import { Home } from './view/pages/Home';
 
 /**
  * The complete app, don't remove this!
@@ -18,14 +25,25 @@ export class GreenifyApp extends LitElement {
 
   render() {
     return html`
-      <main id="main-content">
-        <slot></slot>
-      </main>
+      <gi-header></gi-header>
+      <profile-panel></profile-panel>
+
+      <div id="main-content" class="container">
+        <main id="outlet"/>
+      </div>
     `
   }
 
-  createRenderRoot() {
-    return this;
+  firstUpdated() {
+    const BASE_URL = import.meta.env.BASE_URL;
+    const router = new Router(this.shadowRoot.getElementById('outlet'), {
+      baseUrl: BASE_URL
+    });
+    router.setRoutes([
+      { path: `${BASE_URL}`, component: 'gi-home' },
+      { path: `${BASE_URL}/login`, component: 'gi-login' },
+      // TODO: Include all URLs
+    ])
   }
 }
 
