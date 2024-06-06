e8cdc23c12b2a3db1c32439cabfab569abec4a10
RobinHana04 <robinhana2004@gmail.com>
2024-06-06 19:00:51 +0200

    Finally made calculateResult component

--- server/src/main/java/nl/hu/greenify/core/presentation/dto/InterventionDto.java
+++ server/src/main/java/nl/hu/greenify/core/presentation/dto/InterventionDto.java
@@ -1,11 +1,12 @@
 package nl.hu.greenify.core.presentation.dto;
 
 import lombok.Getter;
-import nl.hu.greenify.core.domain.Intervention;
-import nl.hu.greenify.core.domain.Person;
-import nl.hu.greenify.core.domain.Phase;
+import nl.hu.greenify.core.domain.*;
+import nl.hu.greenify.core.domain.factor.Factor;
+import nl.hu.greenify.core.domain.factor.Subfactor;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Getter
@@ -15,27 +16,52 @@ public class InterventionDto {
     private final String description;
     private final Phase currentPhase;
     private final int surveyAmount;
+    private final double progress;
 
-    public InterventionDto(Long id, String name, String description, Phase currentPhase, int surveyAmount) {
+    public InterventionDto(Long id, String name, String description, Phase currentPhase, int surveyAmount, double progress) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.currentPhase = currentPhase;
         this.surveyAmount = surveyAmount;//NOTE: getSurveys() change to SurveysOfPerson
+        this.progress = progress;
     }
 
 
     public static InterventionDto fromEntity(Intervention intervention, Person person) {
         if(intervention.getCurrentPhase() == null) {
-           return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), null, 0);
+           return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), null, 0, 0);
         }
 
-        int surveyAmount = intervention.getAllSurveysOfParticipant(person).size();
+        List<Survey> surveys = intervention.getAllSurveysOfParticipant(person);
+        int surveyAmount = surveys.size();
+        calculateResult(surveys);
 
-        return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), intervention.getCurrentPhase(), surveyAmount); //NOTE: Should be surveys of person, not all surveys
+        return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), intervention.getCurrentPhase(), surveyAmount, calculateResult(surveys)); //NOTE: Should be surveys of person, not all surveys
     }
 
     public static List<InterventionDto> fromEntities(List<Intervention> interventions, Person person) {
         return interventions.stream().map(intervention -> InterventionDto.fromEntity(intervention, person)).collect(Collectors.toList());
+    }
+
+    private static double calculateResult(List<Survey> surveys) {
+        List<Factor> factors = surveys.stream()
+                .map(Survey::getCategories)
+                .flatMap(List::stream)
+                .map(Category::getFactors)
+                .flatMap(List::stream)
+                .toList();
+
+        List<Subfactor> subfactors = factors.stream()
+                .map(Factor::getSubfactors)
+                .flatMap(List::stream)
+                .toList();
+
+        List<Response> responses = subfactors.stream()
+                .map(Subfactor::getResponse)
+                .filter(Objects::nonNull)  // Exclude null responses
+                .toList();
+
+        return ((double) responses.size() / subfactors.size()) * 100;
     }
 }
