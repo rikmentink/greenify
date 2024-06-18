a844a54f81fe422e82b29d74be0e9311aa066cd0
rikmentink <rik.mentink@icloud.com>
2024-06-18 11:58:41 +0200

    Improved data fetching for overview page.

--- client/src/services/OverviewService.js
+++ client/src/services/OverviewService.js
@@ -1,17 +1,19 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-async function getOverview(id, page = 1, pageSize = 1000) {
-    const url = new URL(`${API_URL}/phase/${id}`);
-    url.searchParams.append('page', page);
-    url.searchParams.append('pageSize', pageSize);
-
-    return fetch(url, {
+async function getOverview(id) {
+    return fetch(`${API_URL}/phase/${phaseId}`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
-    }).then(response => response.json())
-        .catch(error => {
-            console.error(error);
-        });
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Could not fetch phase data. HTTP Error ${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
 export { getOverview };
--- client/src/view/pages/Overview.js
+++ client/src/view/pages/Overview.js
@@ -1,16 +1,11 @@
 import { css, html, LitElement } from 'lit';
 import { getOverview } from '../../services/OverviewService.js';
+import { globalStyles } from '../../assets/global-styles.js';
 
 export class Overview extends LitElement {
-    /**
-     * TODO: Make dynamic
-     * 
-     * 1. Retrieve phase ID from URL
-     * 2. Fetch data from API asynchronously
-     * 3. Render data in the template
-     */
-    
-    static styles = [css`
+    static styles = [
+      globalStyles,
+      css`
       p, h1, h3, em {
         color: black;
       }
@@ -59,55 +54,76 @@ export class Overview extends LitElement {
     `];
 
     static properties = {
-        survey: { type: Object },
+        id: { type: Number },
     };
 
     constructor() {
         super();
-        this.id = 1;
-        this.page = 1;
-        this.pageSize = 10;
-        this.survey = {};
+        this.id = 0;
+        this.data = {};
     }
 
     async connectedCallback() {
-        super.connectedCallback();
-        await this.fetchSurveyData();
+      super.connectedCallback();
+      this.id = getRouter().location.params.id || 0;
     }
 
-    async fetchSurveyData() {
-        try {
-            const urlParams = new URLSearchParams(window.location.search);
-            this.id = urlParams.get('id');
-            this.survey = await getOverview(this.id, this.page, this.pageSize);
-        } catch (error) {
-            console.error('Error fetching survey data:', error);
+    async authorizeAndRedirect() {
+        if (this.id == 0) {
+            window.location.href = '/';
+            return false;
         }
+
+        const survey = await this._fetchData(this.id);
+        if (survey.hasOwnProperty('error')) {
+            window.location.href = '/';
+            return false;
+        }
+        
+        return true;
+    }
+
+    async _fetchData(id) {
+        this.data = new Task(this, {
+            task: async ([id]) => getOverview(id),
+            args: () => [id]
+        });
+        return await this.data.run();
     }
 
     render() {
+        if (this.data._value.error) {
+            return html`
+                <h1>Overview</h1>
+                <p>An error occured while loading the questions: ${this.data._value.message}</p>`;
+        }
+        console.log(this.data);
         return html`
-            <div class="content">
-                <div class="title-desc">
-                    <h1>${this.survey.name || 'Loading...'}</h1>
-                    <p class="description">${this.survey.description || ''}</p>
-                </div>
-                <hr class="divider">
-                <div>
-                    <h3 class="algemeneinfo">Algemene Informatie</h3>
-                    <div class="grid-container">
-                        <em>Fase</em>
-                        <p class="fase">${this.survey.phase || ''}</p>
-                        <em>Voortgang</em>
-                        <p class="voortgang">beantwoorden vragen</p>
-                    </div>
-                </div>
-            </div>
-            <h1>Categorieën</h1>
+          <h1>Overview</h1>
+          <p>done.</p>
+        `
+        // return html`
+        //     <div class="content">
+        //         <div class="title-desc">
+        //             <h1>${this.survey.name || 'Loading...'}</h1>
+        //             <p class="description">${this.survey.description || ''}</p>
+        //         </div>
+        //         <hr class="divider">
+        //         <div>
+        //             <h3 class="algemeneinfo">Algemene Informatie</h3>
+        //             <div class="grid-container">
+        //                 <em>Fase</em>
+        //                 <p class="fase">${this.survey.phase || ''}</p>
+        //                 <em>Voortgang</em>
+        //                 <p class="voortgang">beantwoorden vragen</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <h1>Categorieën</h1>
 
-            ${this.survey.categories ? this.survey.categories.map(category => html`
-                <greenify-categorybox .category=${category}></greenify-categorybox>`) : html`<p>Loading categories...</p>`}
-        `;
+        //     ${this.survey.categories ? this.survey.categories.map(category => html`
+        //         <greenify-categorybox .category=${category}></greenify-categorybox>`) : html`<p>Loading categories...</p>`}
+        // `;
     }
 }
 
