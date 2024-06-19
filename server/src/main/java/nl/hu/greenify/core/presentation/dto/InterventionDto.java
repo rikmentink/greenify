3bef4615444faabfc41c7580be5bc709741c7f20
RobinHana04 <robinhana2004@gmail.com>
2024-06-19 16:46:28 +0200

    Adjusted InterventionDto

--- server/src/main/java/nl/hu/greenify/core/presentation/dto/InterventionDto.java
+++ server/src/main/java/nl/hu/greenify/core/presentation/dto/InterventionDto.java
@@ -17,11 +17,10 @@ public class InterventionDto {
     private final Phase currentPhase;
     private final int surveyAmount;
     private final double totalSurveyProgress;
-    private final double phaseProgress;
     private final List<Person> participants;
     private final List<Phase> phases;
 
-    public InterventionDto(Long id, String name, String description, Phase currentPhase, int surveyAmount, double totalSurveyProgress, List<Person> participants, List<Phase> phases, double phaseProgress) {
+    public InterventionDto(Long id, String name, String description, Phase currentPhase, int surveyAmount, double totalSurveyProgress, List<Person> participants, List<Phase> phases) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -30,23 +29,19 @@ public class InterventionDto {
         this.totalSurveyProgress = totalSurveyProgress;
         this.participants = participants;
         this.phases = phases;
-        this.phaseProgress = phaseProgress;
     }
 
 
     public static InterventionDto fromEntity(Intervention intervention, Person person) {
         if(intervention.getCurrentPhase() == null) {
-           return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), null, 0, 0, new ArrayList<>(), new ArrayList<>(), 0);
+           return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), null, 0, 0, new ArrayList<>(), new ArrayList<>());
         }
-
         List<Survey> surveys = intervention.getSurveysOfPersonInCurrentPhase(person);
         int surveyAmount = surveys.size() + 1;
 
         double surveyprogress = calculateProgress(surveys);
-        double phaseprogress = calculateProgress(intervention.getAllSurveysOfAllPhases());
 
-
-        return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), intervention.getCurrentPhase(), surveyAmount, surveyprogress, intervention.getParticipants(), intervention.getPhases(), phaseprogress);
+        return new InterventionDto(intervention.getId(), intervention.getName(), intervention.getDescription(), intervention.getCurrentPhase(), surveyAmount, surveyprogress, intervention.getParticipants(), intervention.getPhases());
     }
 
     public static List<InterventionDto> fromEntities(List<Intervention> interventions, Person person) {
