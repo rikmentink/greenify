fc7bb4cb6c235bf9bfaaa533c67f6326ed52e6b0
Joostb26 <bos.joost@outlook.com>
2024-06-24 13:55:46 +0200

    Commented right roles to endpoints (undo the comment later)

--- server/src/main/java/nl/hu/greenify/core/presentation/InterventionController.java
+++ server/src/main/java/nl/hu/greenify/core/presentation/InterventionController.java
@@ -11,6 +11,7 @@ import nl.hu.greenify.core.presentation.dto.InterventionDto;
 import nl.hu.greenify.core.presentation.dto.PhaseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,16 +31,19 @@ public class InterventionController extends Controller {
      * Intervention endpoints
      */
 
+//    @Secured({"ROLE_MANAGER", "ROLE_USER", "ROLE_VUMEDEWERKER"})
     @GetMapping(value="/{id}", produces="application/json")
     public ResponseEntity<?> getInterventionById(@PathVariable("id") Long id) {
         return this.createResponse(this.interventionService.getInterventionById(id));
     }
 
+//    @Secured({"ROLE_MANAGER", "ROLE_VUMEDEWERKER"})
     @PostMapping(value="/{id}/person/{personId}", produces="application/json")
     public ResponseEntity<?> addParticipant(@PathVariable("id") Long id, @PathVariable("personId") Long personId) {
         return this.createResponse(this.interventionService.addParticipant(id, personId), HttpStatus.CREATED);
     }
 
+//    @Secured({"ROLE_MANAGER", "ROLE_VUMEDEWERKER"})
     @GetMapping("/all/{id}")
     public ResponseEntity<?> getAllInterventionsByPerson(@PathVariable("id") Long id) {
         List<Intervention> i = this.interventionService.getAllInterventionsByPerson(id);
@@ -48,6 +52,7 @@ public class InterventionController extends Controller {
         return this.createResponse(InterventionDto.fromEntities(i, person));
     }
 
+//    @Secured({"ROLE_MANAGER", "ROLE_USER", "ROLE_VUMEDEWERKER"})
     @PostMapping(consumes="application/json", produces="application/json")
     public ResponseEntity<?> createIntervention(@RequestBody CreateInterventionDto createInterventionDto) {
         return this.createResponse(
@@ -64,21 +69,24 @@ public class InterventionController extends Controller {
      * Phase endpoints
      */
 
+//    @Secured({"ROLE_MANAGER", "ROLE_USER", "ROLE_VUMEDEWERKER"})
     @GetMapping(value="/phase/{id}", produces="application/json")
     public ResponseEntity<?> getPhaseById(@PathVariable("id") Long id) {
         return this.createResponse(this.interventionService.getPhaseById(id));
     }
-
+//    @Secured({"ROLE_MANAGER", "ROLE_USER", "ROLE_VUMEDEWERKER"})
     @GetMapping(value="/{id}/phases", produces="application/json")
     public ResponseEntity<?> getPhasesByInterventionId(@PathVariable("id") Long id) {
         return this.createResponse(PhaseDto.fromEntities(this.interventionService.getPhasesByIntervention(id)));
     }
 
+//    @Secured({"ROLE_MANAGER", "ROLE_USER", "ROLE_VUMEDEWERKER"})
     @GetMapping(value="/{interventionId}/phase/{phaseId}", produces="application/json")
     public ResponseEntity<?> getPhaseProgress(@PathVariable("interventionId") Long interventionId, @PathVariable("phaseId") Long phaseId) {
         return this.createResponse(this.interventionService.getPhaseProgress(interventionId, phaseId));
     }
 
+//    @Secured({"ROLE_MANAGER", "ROLE_USER", "ROLE_VUMEDEWERKER"})
     @PostMapping(value="/{id}/phase", consumes="application/json", produces="application/json")
     public ResponseEntity<?> addPhase(@PathVariable("id") Long id, @RequestBody CreatePhaseDto createPhaseDto) {
         return this.createResponse(this.interventionService.addPhase(id, createPhaseDto.getPhaseName(), createPhaseDto.getDescription()), HttpStatus.CREATED);
--- server/src/main/java/nl/hu/greenify/core/presentation/MailController.java
+++ server/src/main/java/nl/hu/greenify/core/presentation/MailController.java
@@ -3,6 +3,7 @@ package nl.hu.greenify.core.presentation;
 import nl.hu.greenify.core.application.MailService;
 import nl.hu.greenify.core.presentation.dto.MailDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,7 @@ public class MailController extends Controller {
         this.mailService = mailService;
     }
 
+//    @Secured({"ROLE_MANAGER", "ROLE_VUMEDEWERKER"})
     @PostMapping(consumes="application/json", produces="application/json")
     public ResponseEntity<?> sendEmail(@RequestBody MailDto emailDTO) {
         return this.createResponse(
