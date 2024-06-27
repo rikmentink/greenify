682f4af3454cdfe4e8ce1caa4d242e11b73e5bec
rikmentink <rik.mentink@icloud.com>
2024-06-27 07:08:40 +0200

    Improved other fetch methods

--- client/src/services/AccountService.js
+++ client/src/services/AccountService.js
@@ -2,38 +2,45 @@ import { fetchData } from "../utils/fetch";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-async function handleErrorMessages(response) {
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-    }
-}
-
-
 async function login (email, password) {
-    const response = await fetch(`${API_URL}/account/login`, {
+    return fetch(`${API_URL}/account/login`, {
         method: 'POST',
-        body: JSON.stringify({ email, password })
-    });
-
-    await handleErrorMessages(response);
-
-    let token = response.headers.get('Authorization')
-    token = token.replace('Bearer ', '');
-
-    return token
-}
-
-async function register (email, password, firstName, lastName){
-    const response = await fetch(`${API_URL}/account/register`, {
-        method: 'POST',
-        body: JSON.stringify({ email, password, firstName, lastName}),
+        body: JSON.stringify({ email, password }),
         headers: {
             'Content-Type': 'application/json'
         }
-    });
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Could not fetch data. HTTP Error ${response.status}: ${response.statusText}`);
+        }
 
-    await handleErrorMessages(response);
+        let token = response.headers.get('Authorization');
+        token.replace('Bearer ', '');
+        return token;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+async function register (email, password, firstName, lastName) {
+    return fetch(`${API_URL}/account/login`, {
+        method: 'POST',
+        body: JSON.stringify({ email, password, firstName, lastName }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Could not fetch data. HTTP Error ${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
 
 async function getCurrentUser(){
