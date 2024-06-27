5ff4bccd3b8cb250ab884aba338f28d09783450f
rikmentink <rik.mentink@icloud.com>
2024-06-27 21:40:43 +0200

    Included startup command for creating default template

---
+++ server/src/main/java/nl/hu/greenify/StartupRunner.java
@@ -0,0 +1,23 @@
+package nl.hu.greenify;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import nl.hu.greenify.core.application.SurveyService;
+
+@Component
+@ConditionalOnProperty(name="app.startup.runner.enabled", havingValue="true", matchIfMissing=true)
+public class StartupRunner implements CommandLineRunner {
+    private final SurveyService surveyService;
+
+    public StartupRunner(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.surveyService.createDefaultTemplate();
+        System.out.println("Default survey template created.");
+    }
+}
