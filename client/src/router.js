e23280e345f53ec9f7673e63aa90ca5eddb52d9e
rikmentink <rik.mentink@icloud.com>
2024-06-27 16:28:57 +0200

    Reverted router changes

--- client/src/router.js
+++ client/src/router.js
@@ -7,27 +7,27 @@ let routes = [
   { path: "", component: "gi-login", },
   { path: "/login", component: "gi-login" },
   { path: "/register", component: "gi-register" },
-  { path: "(.*)", action: () => window.location.href = "/" }
-];
+  { path: "(.*)", action: () => window.location.href = "/" }];
 
-export const initRouter = async (outlet) => {
+await getCurrentUser().then(user => {
+  if (user) {
+    routes = [
+      { path: "", component: "gi-home" },
+      { path: "/intervention/:id", component: "gi-intervention" },
+      { path: "/intervention/:id/new-phase", component: "gi-createphase" },
+      { path: "/intervention/:id/new-intervention", component: "gi-createintervention" },
+      { path: "/intervention/:interventionId/phase/:phaseId", component: "gi-overview" },
+      { path: "/phase/:id/report", component: "gi-survey-result-report" },
+      { path: "/tool/:id", component: "gi-survey" },
+      { path: "(.*)", action: () => window.location.href = "/" }];
+  }
+});
+
+export const initRouter = (outlet) => {
   router = new Router(outlet, {
     baseUrl: BASE_URL,
   });
   
-  await getCurrentUser().then((user) => {
-    if (user) {
-      routes = [
-        { path: "", component: "gi-home" },
-        { path: "/intervention/:id", component: "gi-intervention" },
-        { path: "/intervention/:id/new-phase", component: "gi-createphase" },
-        { path: "/intervention/:id/new-intervention", component: "gi-createintervention" },
-        { path: "/intervention/:interventionId/phase/:phaseId", component: "gi-overview" },
-        { path: "/phase/:id/report", component: "gi-survey-result-report" },
-        { path: "/tool/:id", component: "gi-survey" },
-        { path: "(.*)", action: () => window.location.href = "/" }];
-    }
-  });
   router.setRoutes(routes);
 }
 
