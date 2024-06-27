af70c532066dc5d384ba3b26692dbf60a43df87d
rikmentink <rik.mentink@icloud.com>
2024-06-27 06:54:46 +0200

    Added fetch method for data fetches

--- client/src/services/SurveyService.js
+++ client/src/services/SurveyService.js
@@ -1,44 +1,18 @@
+import { fetchData } from "../utils/fetch.js";
 const API_URL = import.meta.env.VITE_API_URL;
 
 async function getSurvey(id, categoryId = 0, page = 1, pageSize = 1000) {
-    const url = new URL(`${API_URL}/survey/${id}/questions`);
-    url.searchParams.append('categoryId', categoryId);
-    url.searchParams.append('page', page);
-    url.searchParams.append('pageSize', pageSize);
-
-    return fetch(url, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(response => {
-        if (!response.ok) {
-            console.error(`Could not load survey data. HTTP Error ${response.status}: ${response.statusText}`);
-        }
-        return response.json();
-    });
+    return fetchData(`/survey/${id}/questions?categoryId=${categoryId}&page=${page}&pageSize=${pageSize}`);
 }
 
 async function saveResponse(id, subfactorId, response) {
-  const url = new URL(`${API_URL}/survey/${id}/response`);
-  return fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ 
-      subfactorId,
-      facilitatingFactor: response.facilitatingFactor,
-      priority: response.priority,
-      comment: response.comment
-    }),
-  })
-  .then(response => {
-      if (!response.ok) {
-          throw new Error(`Could not save response. HTTP Error ${response.status}: ${response.statusText}`);
-      }
-      return response.json();
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  body = {
+    subfactorId,
+    facilitatingFactor: response.facilitatingFactor,
+    priority: response.priority,
+    comment: response.comment
+  }
+  return fetchData(`/survey/${id}/response`, body, 'POST');
 } 
 
 export { getSurvey, saveResponse };
