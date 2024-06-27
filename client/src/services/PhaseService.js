e3419a5c564f1ebf66f7ef28bcfc27aae35c5f65
yourilevoye <114139324+yourilevoye@users.noreply.github.com>
2024-06-27 09:44:52 +0200

    Revert "Implemented reusable fetch method"

--- client/src/services/PhaseService.js
+++ client/src/services/PhaseService.js
@@ -1,11 +1,45 @@
-import { fetchData } from "../utils/fetch";
+const API_URL = import.meta.env.VITE_API_URL;
+
+async function handleErrorMessages(response) {
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+}
 
 async function createPhase(id, phaseName, description) {
-    return fetchData(`/intervention/${id}/phase`, { phaseName, description }, 'POST');
+    const url = new URL(`${API_URL}/intervention/${id}/phase`);
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify({phaseName, description}),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Could not save response. HTTP Error ${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
 
 async function getPhaseById(id) {
-    return fetchData(`/intervention/phase/${id}`);
+    const response = await fetch(`${API_URL}/intervention/phase/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+
+    await handleErrorMessages(response);
+
+    return response.json();
 }
 
 export { getPhaseById, createPhase};
--- client/src/services/SurveyService.js
+++ client/src/services/SurveyService.js
@@ -1,17 +1,50 @@
-import { fetchData } from "../utils/fetch.js";
+const API_URL = import.meta.env.VITE_API_URL;
 
 async function getSurvey(id, categoryId = 0, page = 1, pageSize = 1000) {
-    return fetchData(`/survey/${id}/questions?categoryId=${categoryId}&page=${page}&pageSize=${pageSize}`);
+    const url = new URL(`${API_URL}/survey/${id}/questions`);
+    url.searchParams.append('categoryId', categoryId);
+    url.searchParams.append('page', page);
+    url.searchParams.append('pageSize', pageSize);
+
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.error(`Could not load survey data. HTTP Error ${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+    });
 }
 
 async function saveResponse(id, subfactorId, response) {
-  body = {
-    subfactorId,
-    facilitatingFactor: response.facilitatingFactor,
-    priority: response.priority,
-    comment: response.comment
-  }
-  return fetchData(`/survey/${id}/response`, body, 'POST');
+  const url = new URL(`${API_URL}/survey/${id}/response`);
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+    },
+    body: JSON.stringify({ 
+      subfactorId,
+      facilitatingFactor: response.facilitatingFactor,
+      priority: response.priority,
+      comment: response.comment
+    }),
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error(`Could not save response. HTTP Error ${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+  })
+  .catch(error => {
+    console.error(error);
+  });
 } 
 
 export { getSurvey, saveResponse };
